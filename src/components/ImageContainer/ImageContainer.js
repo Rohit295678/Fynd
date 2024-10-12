8a725d65dae9489265f3c6edd53c4b12f4f5a3cd
Rohit295678 <rohitbkpraj@gmail.com>
2024-10-13 02:24:33 +0530

    update the changes

--- src/components/ImageContainer/ImageContainer.js
+++ src/components/ImageContainer/ImageContainer.js
@@ -1,62 +1,82 @@
-import {React, useState} from 'react'
-
-import Modal from '../Modal/Modal';
+import { React, useState, useEffect } from 'react';
 import './ImageContainer.css';
 
-const ImageContainer = ({images}) => {
-    const [selectedImage, setSelectedImage] = useState(null);
+const ImageContainer = ({ images }) => {
+   console.log(images)
+  const [largeImage, setLargeImage] = useState(images[0]);
+  const [RightLargeImage, setRightLargeImage] = useState(images[4]);
+  console.log(largeImage)
 
-    const largeImage= images[0]
-    const RightLargeImage = images[4]
-    
-  const smallImages=images.slice(1,4);
-  const RightsmallImages=images.slice(5);
+  const [smallImages, setSmallImages] = useState(images.slice(1, 4));
+  const [RightsmallImages, setRightsmallImages] = useState(images.slice(5));
+ 
+  useEffect(() => {
+    setLargeImage(images[0]);
+    setRightLargeImage(images[4]);
+    setSmallImages(images.slice(1, 4));
+    setRightsmallImages(images.slice(5));
+  }, [images]);
 
-  const handleClickImage = (imgSrc) => {
-    setSelectedImage(imgSrc);
+  const handleClickLeftImage = (clickedImage, index) => {
+    const newSmallImages = [...smallImages];
+    newSmallImages[index] = largeImage;
+    setSmallImages(newSmallImages);
+    setLargeImage(clickedImage);
   };
 
-  const closeModal = () => {
-    setSelectedImage(null);
+  const handleClickRightImage = (clickedImage, index) => {
+    const newSmallImages = [...RightsmallImages];
+    newSmallImages[index] = RightLargeImage;
+    setRightsmallImages(newSmallImages);
+    setRightLargeImage(clickedImage);
   };
 
   return (
     <div>
       <div className="images-container">
+      
+        <div className="image-section">
+          <div className="large-image">
+            <img src={largeImage} alt="Large" />
+          </div>
+          <div className="small-images">
+            {smallImages.map((src, index) => (
+              <img
+                key={index}
+                src={src}
+                alt={`Small ${index + 1}`}
+                onClick={() => handleClickLeftImage(src, index)}
+              />
+            ))}
+          </div>
+          <div className='star_container'>
+            <div className='dash dash-top'>----</div>
+            <div className="transform-icon">
+              <img src='https://rideshinemobiledetailing.com/wp-content/uploads/2022/10/star-icon.png' alt="star"/>
+            </div>
+            <div className='dash dash-bottom'>--------<span className='arrow'>></span></div>
+          </div>
+        </div>
 
         <div className="image-section">
-        <div className="large-image">
-          <img src={largeImage} alt="Large" onClick={() => handleClickImage(largeImage)} />
-        </div>
-        <div className="small-images">
-          {smallImages.map((src, index) => (
-            <img key={index} src={src} alt={`Small ${index + 1}`} onClick={() => handleClickImage(src)} />
-          ))}
-        </div>
-        <div className='star_container'>
-        <div className='dash'>----</div>
-        <div className="transform-icon">
-            <img src='https://rideshinemobiledetailing.com/wp-content/uploads/2022/10/star-icon.png' alt="star"/>
-        </div>
-        <div className='dash'>--------<span className='arrow'>></span></div>
-        </div>
-      </div>
-            
-        <div className="image-section">
-        <div className="large-image">
-          <img src={RightLargeImage} alt="Large" onClick={() => handleClickImage(RightLargeImage)} />
-        </div>
-        <div className="small-images-right">
-          {RightsmallImages.map((src, index) => (
-            <img key={index} src={src} alt={`Small ${index + 1}`} className={`small-image-${index + 1}`} onClick={() => handleClickImage(src)} />
-          ))}
+          <div className="large-image">
+            <img src={RightLargeImage} alt="Large" />
+          </div>
+          <div className="small-images-right">
+            {RightsmallImages.map((src, index) => (
+              <img
+                key={index}
+                src={src}
+                alt={`Small ${index + 1}`}
+                className={`small-image-${index + 1}`}
+                onClick={() => handleClickRightImage(src, index)}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      </div>
-
-      {selectedImage && <Modal src={selectedImage} closeModal={closeModal} />}
     </div>
-  )
-}
+  );
+};
 
-export default ImageContainer
+export default ImageContainer;
